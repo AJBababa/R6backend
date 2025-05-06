aa3f9902ff89d3ecafa7d721b11ae98f657c72ee
AJBababa <alvarojbustos@gmail.com>
2025-05-06 15:56:54 +0200

    updated to render

--- src/db-connection.ts
+++ src/db-connection.ts
@@ -1,5 +1,8 @@
 import { Pool } from 'pg';
 
+
+//localhost
+/*
 const pool = new Pool({
   user: 'postgres',
   password: 'admin',
@@ -7,6 +10,18 @@ const pool = new Pool({
   port: 5432, // default Postgres port
   database: 'R6SmashOrPass'
 });
+*/
+
+//render
+
+const pool = new Pool({
+  user: 'r6smashorpassdb_user',
+  password: 'rcNq7PaQj9RaXQA3Gu6zCYBMyLCQ08Ai',
+  host: 'dpg-d08eu0fgi27c738hedh0-a',
+  port: 5432, // default Postgres port
+  database: 'r6smashorpassdb'
+});
+
 
 export function query(text: any, p0?: any[]): any {
     return pool.query(text);
